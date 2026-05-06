a759f1d19aee0e4e22f1bfdd64af09979da40c6f
ltpham123 <ltpham123@gmail.com>
2026-05-06 12:27:27 -0500

    pre-class

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,16 @@
+########################
+### YUU Data Academy ###
+### Longley Pham     ###
+### Exercise 1B      ###
+########################
+
+customer_id = 3385                  #Unique identifier, should be a number for base cases, but can be alphanumeric
+customer_name = 'James Leroy'       #string; first, last but can be separated
+customer_gender = ''                #string, should be select by case
+customer_dob = '5/13/2003'          #string if you want to include hyphens and slashes; numeric if separated
+dl_number = 'A90234890234'          #numeric with the exception of the first/last placeholder
+autopolicy_number = 790252          #can be alphanumeric
+
+self_name = 'Longley'
+self_city = 'Chicago'
+self_state = 'Illinois'
