e73c0ffc5aaca238527eed64fb943eade2c7aac3
ltpham123 <ltpham123@gmail.com>
2026-05-07 11:38:55 -0500

    net worth brainstorming

---
+++ week-05/Ex2A_MathScripts.py
@@ -0,0 +1,5 @@
+# a) What are "assets" that would need to be included in this calculation? What about "debts"?
+# Assets include cash, bank balances, investments, property, and any other valuable possessions.
+# Debts include loans, credit card balances, mortgages, and any other financial obligations.
+
+# b) Net worth = Total Assets - Total Debts
