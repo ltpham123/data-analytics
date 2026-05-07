8277e0c7d84b535ec1c9bc889383b83797f14264
ltpham123 <ltpham123@gmail.com>
2026-05-07 12:14:01 -0500

    area of a circle script

--- week-05/area_of_circle.py
+++ week-05/area_of_circle.py
@@ -2,3 +2,8 @@
 # just the day). Figure out the formula, refresh your recollection of the difference
 # between diameter and radius, and figure out what the script should look like.
 # The area of a circle with radius [number] is [number]
+
+diameter = 13  # Example day of the month (13th)
+radius = diameter / 2
+area = 3.14159 * (radius ** 2)
+print(f"The area of a circle with radius {radius} is {area}") # Measurements are based in 'units'
