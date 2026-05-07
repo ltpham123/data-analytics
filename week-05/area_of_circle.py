e08fc6d92d456a3647c0ce20cd2bae7eef059488
ltpham123 <ltpham123@gmail.com>
2026-05-07 12:51:04 -0500

    updated to include the input function

--- week-05/area_of_circle.py
+++ week-05/area_of_circle.py
@@ -3,7 +3,8 @@
 # between diameter and radius, and figure out what the script should look like.
 # The area of a circle with radius [number] is [number]
 
-diameter = 13  # Example day of the month (13th)
+# diameter = 13  # Example day of the month (13th)
+diameter = int(input("What is your day of birth? "))  # Get the diameter from user input; initially a string, so convert to integer
 radius = diameter / 2
 area = 3.14159 * (radius ** 2)
-print(f"The area of a circle with radius {radius} is {area}") # Measurements are based in 'units'
+print(f"The area of a circle with radius {format(radius, '.2f')} is {format(area, '.2f')}")  # Measurements are based in 'units'
