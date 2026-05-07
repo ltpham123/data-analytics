8fc43fd0556e40975815f88185d5560a4ce1fcbc
ltpham123 <ltpham123@gmail.com>
2026-05-07 11:43:04 -0500

    formatting for decimal values

--- week-05/net_worth.py
+++ week-05/net_worth.py
@@ -6,6 +6,6 @@ assets = 100000  # Example total assets
 debts = 50000    # Example total debts
 net_worth = assets - debts
 
-print(f"Your total assets are {assets}")
-print(f"Your total debts are {debts}")
-print(f"Your net worth is {net_worth}")
+print(f"Your total assets are {assets:.2f}")
+print(f"Your total debts are {debts:.2f}")
+print(f"Your net worth is {net_worth:.2f}")
