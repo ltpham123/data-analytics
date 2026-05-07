e1abd12741c86402d4d34c2fa2729519d5829d1e
ltpham123 <ltpham123@gmail.com>
2026-05-07 12:31:33 -0500

    updated rule of 72 to meet requirements

--- week-05/rule_of_72.py
+++ week-05/rule_of_72.py
@@ -5,10 +5,22 @@
 # worth [number] in [number] years
 # rule of 72: years to double = 72 / interest rate
 
-interest_rate = 5  # Example interest rate in percent
-current_savings = 10000  # Example current savings
-years_to_double = 72 / interest_rate
+# Show your doubled balance with 2 digits to the right of the decimal point by using
+# format(__, ".2f") and show years with 1 digit to the right of the decimal. How
+# can you do this using format()?
 
-print(f"Your current savings is {current_savings:.2f}.")
-print(f"At a {interest_rate}% interest rate, your savings account will be")
-print(f"worth {current_savings*2:.2f} in {years_to_double:.2f} years.")
+# There are a couple ways you might get the interest rate to display as a percentage.
+# One option is to use the format function. In this case, instead of including the
+# character f to assign a fixed decimal format, use the character % to assign the
+# percentage format, e.g. format(__, ".0%")
+
+interest_rate = 0.05 # 5  # Example interest rate in percent
+current_savings = 10000  # Example current savings
+years_to_double = 72 / (interest_rate * 100)
+
+# print(f"Your current savings is {current_savings:.2f}.")
+# print(f"At a {interest_rate}% interest rate, your savings account will be")
+# print(f"worth {current_savings*2:.2f} in {years_to_double:.2f} years.")
+print(f"Your current savings is {format(current_savings, '.2f')}.")
+print(f"At a {format(interest_rate, '.0%')} interest rate, your savings account will be")
+print(f"worth {format(current_savings*2, '.2f')} in {format(years_to_double, '.1f')} years.")
