9c37d4f7a42d8d337baf3bd60365c17d17e0c1c8
ltpham123 <ltpham123@gmail.com>
2026-05-07 11:45:50 -0500

    area of a rectangle script

---
+++ week-05/area_of_rectangle.py
@@ -0,0 +1,16 @@
+# Say you have a rectangle that has dimensions corresponding to your birthday – the
+# month number is one side and the day of the month is the other side. How would
+# you calculate the area of this rectangle?
+
+# Side A is [number]
+# Side B is [number]
+# The area of the rectangle is [number]
+
+side_a = 7  # Example month number (July)
+side_b = 13  # Example day of the month
+area = side_a * side_b
+
+# Measurements are based in 'units'
+print(f"Side A is {side_a}")
+print(f"Side B is {side_b}")
+print(f"The area of the rectangle is {area}")
