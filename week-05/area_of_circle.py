96b911db3c4e1f442c55a85eac07225e6640942a
ltpham123 <ltpham123@gmail.com>
2026-05-07 12:12:19 -0500

    initialize area of a circle script

---
+++ week-05/area_of_circle.py
@@ -0,0 +1,4 @@
+# The diameter of a given circle is the same as the day of your birthday (not the month,
+# just the day). Figure out the formula, refresh your recollection of the difference
+# between diameter and radius, and figure out what the script should look like.
+# The area of a circle with radius [number] is [number]
