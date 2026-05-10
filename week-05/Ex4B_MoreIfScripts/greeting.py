8d666968661caa88edd8ac4b75920741b45b669f
ltpham123 <ltpham123@gmail.com>
2026-05-10 18:55:45 -0500

    added bonus condition

--- week-05/Ex4B_MoreIfScripts/greeting.py
+++ week-05/Ex4B_MoreIfScripts/greeting.py
@@ -12,11 +12,14 @@ current_time = datetime.now().time()  # Get the current time
 # current_time = datetime.strptime("09:30:00", "%H:%M:%S").time()  # Morning
 # current_time = datetime.strptime("14:00:00", "%H:%M:%S").time()  # Day
 # current_time = datetime.strptime("18:00:00", "%H:%M:%S").time()  # Evening
+# current_time = datetime.strptime("23:30:00", "%H:%M:%S").time()  # wydusl
+# current_time = datetime.strptime("06:30:00", "%H:%M:%S").time()  # back to Morning
 
 if current_time < datetime.strptime("10:00:00", "%H:%M:%S").time():
     print("Good morning!")
 elif current_time < datetime.strptime("17:00:00", "%H:%M:%S").time():
     print("Good day!")
-else:
+elif current_time < datetime.strptime("23:00:00", "%H:%M:%S").time():  # updated to include 11pm
     print("Good evening!")
-
+elif current_time > datetime.strptime("23:00:00", "%H:%M:%S").time() or current_time < datetime.strptime("04:00:00", "%H:%M:%S").time():  # updated to include 11pm and 4am
+    print("What are you doing up so late??")
