fd4497a13cc595163fb0b700c909e2fd0196ae8d
ltpham123 <ltpham123@gmail.com>
2026-05-11 09:20:30 -0500

    major script

--- week-05/Ex4B_MoreIfScripts/show_major.py
+++ week-05/Ex4B_MoreIfScripts/show_major.py
@@ -2,8 +2,42 @@
 #   office based on the major code
 
 student_name = input("Enter student name: ")
-student_major = input("Enter major code: ")
+student_major = input("Enter major code: ").upper()
 
 #   dictionary, initialize blanks: <unknown>
 #   for major name and nothing for location
 #   Serves as alternate output for invalid codes
+
+major_directory = {
+    "BIOL": {
+        "major": "Biology",
+        "location": "Science Bldg, Room 310"
+        },
+    "CSCI": {
+        "major": "Computer Science",
+        "location": "Sheppard Hall, Room 314"
+        },
+    "ENG": {
+        "major": "English",
+        "location": "Kerr Hall, Room 201"
+        },
+    "HIST": {
+        "major": "History",
+        "location": "Kerr Hall, Room 114"
+        },
+    "MKT": {
+        "major": "Marketing",
+        "location": "Westly Hall, Room 310"
+        }
+}
+
+if student_major in major_directory:
+    major_name = major_directory[student_major]["major"]
+    major_location = major_directory[student_major]["location"]
+else:
+    major_name = "<unknown>"
+    major_location = ""
+
+print(f"Student: {student_name}")
+print(f"Major: {major_name}")
+print(f"Office Location: {major_location}")
