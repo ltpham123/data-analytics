9e78132dac2ea3cbcdc56d92f1e1f3157db31fa2
ltpham123 <ltpham123@gmail.com>
2026-05-11 10:24:41 -0500

    total sales calculated (bonus)

--- week-05/Ex4C_LoopScripts/sales_performance.py
+++ week-05/Ex4C_LoopScripts/sales_performance.py
@@ -6,7 +6,12 @@ sales_data = [
     ('Bob Nguyen',       'West',     4980.25),
 ]
 
+total_sales = 0
+
 for name, region, sales in sales_data:
+    total_sales += sales
     print(f"{name} ({region}): ${sales:.2f}")
     if sales >= 5000:
         print(f"  ^ Top performer!")
+
+print(f"\nTotal sales: ${total_sales:.2f}")
