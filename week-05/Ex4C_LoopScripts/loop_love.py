f31a42e44ae50c41e507ce2d1e11a6df6281fe3a
ltpham123 <ltpham123@gmail.com>
2026-05-11 09:31:57 -0500

    love loop script

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,6 @@
+great_synonyms = ["amazing", "wonderful", "fantastic", "incredible", "marvelous", "excellent"]
+
+for synonym in great_synonyms:
+    print(f"Loops are {synonym}!")
+
+print(f"I <3 loops")
