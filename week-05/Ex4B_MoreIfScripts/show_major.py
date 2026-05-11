30fe3719af9f75885178ea93c62342a930082067
ltpham123 <ltpham123@gmail.com>
2026-05-10 21:26:42 -0500

    initialize student directory

---
+++ week-05/Ex4B_MoreIfScripts/show_major.py
@@ -0,0 +1,9 @@
+#   Display the name of the major and location of the department's
+#   office based on the major code
+
+student_name = input("Enter student name: ")
+student_major = input("Enter major code: ")
+
+#   dictionary, initialize blanks: <unknown>
+#   for major name and nothing for location
+#   Serves as alternate output for invalid codes
