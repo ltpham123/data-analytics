d8201a0b97ec325149f120cc2ae10ffe8cfdb7f7
ltpham123 <ltpham123@gmail.com>
2026-05-11 13:33:20 -0500

    input validation and while loop implemented to prompt for valid input

--- week-05/Ex4C_LoopScripts/Ex4D_Bonus_number_guesser.py
+++ week-05/Ex4C_LoopScripts/Ex4D_Bonus_number_guesser.py
@@ -8,16 +8,28 @@ rng_target = int(list(rng_pool)[0])  # converts a set to a list
 
 guess_list = []
 
-rng_guess = int(input("Guess a number between 1 and 100: "))
+while True:
+    rng_guess = input("Guess a number between 1 and 100: ")
+    if rng_guess.isdigit() and int(rng_guess) in range(1, 101):
+        break
+    else:
+        print("Invalid input.")
+
+# rng_guess = int(input("Guess a number between 1 and 100: "))
 num_guesses = 1
 guess_list.append(rng_guess)
 
-while rng_guess != rng_target:
-    if rng_guess < rng_target:
+while int(rng_guess) != rng_target:
+    if int(rng_guess) < rng_target:
         print("Too low! Try again.")
     else:
         print("Too high! Try again.")
-    rng_guess = int(input("Guess a number between 1 and 100: "))
+    while True:
+        rng_guess = input("Guess a number between 1 and 100: ")
+        if rng_guess.isdigit() and int(rng_guess) in range(1, 101):
+            break
+        else:
+            print("Invalid input.")
     num_guesses += 1
     guess_list.append(rng_guess)
 
@@ -25,4 +37,3 @@ if num_guesses < 5:
     print("You're awesome!")
 print(f"Congratulations! You guessed the number {rng_target} correctly!")
 print(f"It took you {num_guesses} guesses: {guess_list}")
-
