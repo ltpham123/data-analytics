958f0963658f426bb0221ac66bc37234fb4ca442
ltpham123 <ltpham123@gmail.com>
2026-05-20 14:56:50 -0500

    added notes

--- week-06/Ex2B_Functions/sampling_tools.py
+++ week-06/Ex2B_Functions/sampling_tools.py
@@ -6,11 +6,10 @@ potd = random.choice(products)
 survey_products = random.sample(products, 3)
 
 random.shuffle(products)
-shuffled_products = products
 
 daily_count = random.randint(50, 300)
 
-print(f"Product of the Day: {potd}")
-print(f"Three products for survey: {survey_products[0]}, {survey_products[1]}, and {survey_products[2]}")
-print(f"Shuffled products: {shuffled_products}")
-print(f"Daily sales count: {daily_count}")
+print(f"Product of the Day: {potd}")  # picks one random product
+print(f"Three products for survey: {survey_products[0]}, {survey_products[1]}, and {survey_products[2]}")  # picks three random products without duplicates
+print(f"Shuffled products: {products}")  # shuffles list and prints new list
+print(f"Daily sales count: {daily_count}")  # generates a random integer from the specified range
