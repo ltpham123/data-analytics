7369e13d219335d4031c854c36c54b966cefd0c2
ltpham123 <ltpham123@gmail.com>
2026-05-24 05:21:58 -0500

    stopped at step 15 for read and write files lab

---
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -0,0 +1,3 @@
+test_file = open("about_me.txt", "a")
+test_file.write("I would go straight to the hardware store and check out what new tools they had. Of course, mine would already be the best!\n")  # adjusted to add a new line after input
+test_file.close()
